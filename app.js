402a4abb2fa6ed9b9a6495a138b5aa9d2cedb52f
Rishavrishu-creator <66913014+Rishavrishu-creator@users.noreply.github.com>
2020-10-05 04:11:48 +0530

    rr

--- app.js
+++ app.js
@@ -71,10 +71,12 @@ io.on('connection',function(socket){
         })
 
         socket.on('decline',function(data){
+            console.log(array4)
             var i = array4.indexOf(data.to)
             var j= array4.indexOf(data.sender)
             array4.splice(i,1)
             array4.splice(j,1)
+            console.log(array4)
 
             io.to(data.to).emit("declined-accepted",data)
         })
